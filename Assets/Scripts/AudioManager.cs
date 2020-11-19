b4c0a540ed23476379a4909aff5921abb735f57e
Nero Leung <nerolileung@gmail.com>
2020-11-19 01:24:28 +0000

    recovered audio stuff

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -21,16 +21,29 @@ public class AudioManager : MonoBehaviour
     [SerializeField]
     private Text musicText;
 
+    private AudioClip[] musicFiles = new AudioClip[2];
+    private float musicVolOrig;
+    private float musicTimerCurrent;
+    private static float musicTimerMax;
+    private AudioClip musicCurrent;
+
     // Start is called before the first frame update
     void Start()
     {
         mixer = Resources.Load<AudioMixer>("Audio/MainMixer");
+        musicFiles[0] = Resources.Load<AudioClip>("Audio/RelaxingPianoMusic");
+        musicFiles[1] = Resources.Load<AudioClip>("Audio/UnnaturalSituation");
+        musicTimerMax = 2f;
+        musicTimerCurrent = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (musicTimerCurrent > 0) {
+            musicTimerCurrent -= Time.deltaTime;
+
+        }
     }
 
     public void ChangeMusicVol(float value){
@@ -44,5 +57,44 @@ public class AudioManager : MonoBehaviour
     public void ChangeVoiceVol(float value){
         mixer.SetFloat("volVoice",value);
         voiceText.text = Mathf.RoundToInt(value+80).ToString()+"%";
+    }
+
+    public void PlayMusic(string clipName){
+        int index = musicFiles.Length;
+        switch (clipName){
+            case "RelaxingPianoMusic":
+                index = 0;
+            break;
+            case "UnnaturalSituation":
+                index = 1;
+            break;
+            default:
+                Debug.Log("Unknown bgm file: "+clipName);
+            break;
+        }
+
+        if (index < musicFiles.Length){
+            musicCurrent = musicFiles[index];
+            if (musicSource.isPlaying){
+                musicTimerCurrent = musicTimerMax;
+                mixer.GetFloat("volBGM", out musicVolOrig);
+            }
+            else {
+                musicSource.clip = musicCurrent;
+                musicSource.Play();
+            }
+        }
+
+    }
+    public void PlayVoice(AudioClip clip){
+        voiceSource.clip = clip;
+        if (!voiceSource.isPlaying)
+            voiceSource.Play();
+    }
+    public bool isVoicePlaying(){
+        return voiceSource.isPlaying;
+    }
+    public void StopVoice(){
+        voiceSource.Stop();
     }
 }
