279c0c1a4f89cc4ed6adb492de52255084b3def2
Nero Leung <nerolileung@gmail.com>
2020-11-19 02:04:41 +0000

    rebuilt state machine
    framework is BACK BABY

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class GameManager : MonoBehaviour
 {
-    private static bool gameActive;
+    private static STATE gameState;
+    enum STATE {
+        MENU, DIALOGUE, CHOICE, TRANS_OUT, TRANS_IN, ENDING
+    }
+    private AudioManager audioManager;
     private List<Passage> passageBuffer = new List<Passage>();
     [SerializeField]
     private Text dialogue; 
-    private AudioManager audioManager;
-
+    #region timers
     [SerializeField]
     private GameObject choiceTimerUI;
     private Text choiceTimerText;
@@ -18,8 +22,8 @@ public class GameManager : MonoBehaviour
     private static float choiceTimerMax;
     private float fadeTimerCurrent;
     private static float fadeTimerMax;
-    private static bool fadeOut;
-
+    #endregion
+    #region ui connections
     [SerializeField]
     private GameObject skipButton;
     [SerializeField]
@@ -30,6 +34,7 @@ public class GameManager : MonoBehaviour
     private GameObject mainMenu;
     [SerializeField]
     private GameObject gameCanvas;
+    #endregion
 
     // Start is called before the first frame update
     void Start()
@@ -41,98 +46,53 @@ public class GameManager : MonoBehaviour
         choiceTimerMax = 5.0f;
         choiceTimerCurrent = 0f;
         choiceTimerText = choiceTimerUI.GetComponentInChildren<Text>();
-        choiceTimerUI.SetActive(false);
 
         fadeTimerMax = 1.0f;
         fadeTimerCurrent = 0f;
-        fadeOut = true;
-
-        gameActive = false;
+        
+        gameState = STATE.MENU;
     }
 
     // Update is called once per frame
     void Update()
     {
         // return to main menu using esc
-        if (Input.GetKeyDown(KeyCode.Escape)){
-            if (!mainMenu.activeInHierarchy){
-                gameActive = false;
-                audioManager.StopVoice();
-                // audioManager.playMusic("menuMusic");
-                gameCanvas.SetActive(false);
-                mainMenu.SetActive(true);
-            }
+        if (Input.GetKeyDown(KeyCode.Escape) && gameState != STATE.MENU){
+            ChangeState(STATE.MENU);
         }
 
-        if (gameActive){
-            // countdown
-            if (choiceTimerCurrent > 0){
-                choiceTimerCurrent -= Time.deltaTime;
-                choiceTimerUI.GetComponent<Image>().fillAmount = choiceTimerCurrent/choiceTimerMax;
-                choiceTimerText.text = Mathf.CeilToInt(choiceTimerCurrent).ToString();
-
-                if (Input.anyKeyDown && passageBuffer[0].next2!=null){
-                    // ignore mouse and escape
-                    if (!Input.GetMouseButtonDown(0) && !Input.GetMouseButtonDown(1) && !Input.GetMouseButtonDown(2)) {
-                            EndPassage(false);
-                    }
-                }
-                
-                if (choiceTimerCurrent <= 0){
-                    choiceTimerUI.SetActive(false);
-                    EndPassage(true);
-                }
-            }
-            else {
+        switch (gameState){
+            case STATE.DIALOGUE:
                 // has passage finished?
                 if (!audioManager.isVoicePlaying()){
-                    if (passageBuffer[0].next2 != null){
-                        choiceTimerCurrent = choiceTimerMax;
-                        choiceTimerUI.SetActive(true);
-                    }
-                    else if (passageBuffer[0].next1 != null){
+                    if (passageBuffer[0].next2 != null) // choice available!
+                        ChangeState(STATE.CHOICE);
+                    else if (passageBuffer[0].next1 != null) // go to next passage
                         EndPassage(true);
-                    }
-                    else {
-                        // this is an ending!
-                        choiceTimerCurrent = 0f;
-                        skipButton.SetActive(false);
-                        menuButton.SetActive(true);
-                    }
+                    else // this is an ending!
+                        ChangeState(STATE.ENDING);
                 }
-            }
-
-            // passage transition out/in
-            if (fadeTimerCurrent > 0){
-                fadeTimerCurrent -= Time.deltaTime;
-                Color fadeColour = dialogue.color;
-                if (fadeOut){
-                    fadeColour.a = Mathf.Lerp(0,1,fadeTimerCurrent/fadeTimerMax);
-                    if (fadeTimerCurrent <= 0) {
-                        PlayPassage();
-                        
-                        fadeColour.a = 1;
-                        fadeOut = false;
-                        fadeTimerCurrent = fadeTimerMax;
-                    }
-                }
-                else fadeColour.a = Mathf.Lerp(1,0,fadeTimerCurrent/fadeTimerMax);
-                
-                dialogue.color = fadeColour;
-            }
+            break;
+            case STATE.CHOICE:
+                ChoiceTimerTick();
+            break;
+            case STATE.TRANS_IN:
+            case STATE.TRANS_OUT:
+                // passage transition out/in
+                FadeTimerTick();
+            break;
         }
     }
 
     public void Init(){
         passageBuffer.Clear();
-        menuButton.SetActive(false);
-        skipButton.SetActive(true);
-        
         LoadPassage("Explanation");
         LoadPassage(passageBuffer[0].next1.name);
         LoadPassage(passageBuffer[0].next2.name);
         PlayPassage();
+        ChangeState(STATE.DIALOGUE);
     }
+    #region passage actions
     private void LoadPassage(string passName){
         for (int i = 0; i < passageBuffer.Count; i++){
             if (passageBuffer[i].name.Equals(passName)) return;
@@ -159,22 +119,83 @@ public class GameManager : MonoBehaviour
         
         // nextPassage should be at index 0 of buffer
         if (passageBuffer[0].next2 != null) LoadPassage(passageBuffer[0].next2.name);
-        if (passageBuffer[0].next1 != null) {
-            LoadPassage(passageBuffer[0].next1.name);
-            // fade out current text
-                fadeTimerCurrent = fadeTimerMax;
-                fadeOut = true;
-        }
+        if (passageBuffer[0].next1 != null) LoadPassage(passageBuffer[0].next1.name);
+        
+        // fade out current text
+        ChangeState(STATE.TRANS_OUT);
+        
     }
-
     public void SkipPassage(){
-        if (choiceTimerCurrent > 0) { // in countdown
+        if (gameState == STATE.CHOICE) {
             EndPassage(true);
         }
-        else audioManager.StopVoice();
+        else audioManager.StopVoice(); // go to end of dialogue logic
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+    #endregion
+
+    private void ChoiceTimerTick(){
+        choiceTimerCurrent -= Time.deltaTime;
+        choiceTimerUI.GetComponent<Image>().fillAmount = choiceTimerCurrent/choiceTimerMax;
+        choiceTimerText.text = Mathf.CeilToInt(choiceTimerCurrent).ToString();
+
+        if (Input.anyKeyDown && (!Input.GetMouseButtonDown(0) && !Input.GetMouseButtonDown(1) && !Input.GetMouseButtonDown(2))) {
+            EndPassage(false);
+        }
+        if (choiceTimerCurrent <= 0){
+            EndPassage(true);
+        }
+    }
+    private void FadeTimerTick(){
+        fadeTimerCurrent -= Time.deltaTime;
+        Color fadeColour = dialogue.color;
+        if (gameState==STATE.TRANS_OUT){
+            fadeColour.a = Mathf.Lerp(0,1,fadeTimerCurrent/fadeTimerMax);
+            if (fadeTimerCurrent <= 0) ChangeState(STATE.TRANS_IN);
+        }
+        else {
+            fadeColour.a = Mathf.Lerp(1,0,fadeTimerCurrent/fadeTimerMax);
+            if (fadeTimerCurrent <= 0) ChangeState(STATE.DIALOGUE);
+        }
+        dialogue.color = fadeColour;
     }
 
-    public void ToggleGame(){
-        gameActive = !gameActive;
+    public void GameToMenu(){
+        ChangeState(STATE.MENU);
+    }
+    private void ChangeState(STATE state){
+        switch (state){
+            case STATE.MENU:
+                audioManager.StopVoice();
+                // audioManager.playMusic("menuMusic");
+                gameCanvas.SetActive(false);
+                mainMenu.SetActive(true);
+            break;
+            case STATE.DIALOGUE:
+                choiceTimerUI.SetActive(false);
+                skipButton.SetActive(true);
+                menuButton.SetActive(false);
+            break;
+            case STATE.CHOICE:
+                choiceTimerUI.SetActive(true);
+                choiceTimerCurrent = choiceTimerMax;
+            break;
+            case STATE.TRANS_OUT:
+                if (gameState == STATE.CHOICE){
+                    choiceTimerUI.SetActive(false);
+                }
+                skipButton.SetActive(false);
+                fadeTimerCurrent = fadeTimerMax;
+            break;
+            case STATE.TRANS_IN:
+                PlayPassage();
+                fadeTimerCurrent = fadeTimerMax;
+            break;
+            case STATE.ENDING:
+                skipButton.SetActive(false);
+                menuButton.SetActive(true);
+            break;
+        }
+        gameState = state;
     }
 }
